5a1c71a49dd96e1deb6346af29ecfa8baf0fd58f
Travis Holton <travis@ideegeo.com>
2015-11-02 09:19:02 +1300

    use built-in django test script tool

--- run_tests.sh
+++ run_tests.sh
@@ -2,11 +2,12 @@
 
 set -e
 
-nosetests -v --with-doctest \
-    --with-xunit --xunit-file=xunit.xml \
-    --with-coverage --cover-html --cover-html-dir=htmlcov \
-    --cover-xml --cover-xml-file=coverage.xml \
-    "$@" kube_cd_project/ polls/
+python3 manage.py test polls
+#nosetests -v --with-doctest \
+    #--with-xunit --xunit-file=xunit.xml \
+    #--with-coverage --cover-html --cover-html-dir=htmlcov \
+    #--cover-xml --cover-xml-file=coverage.xml \
+    #"$@" kube_cd_project/ polls/
 
 #2to3 -j 3 -wn -f all -f idioms -f buffer -f set_literal -f ws_comma api/
 autopep8 --in-place --aggressive -r kube_cd_project/ polls/
