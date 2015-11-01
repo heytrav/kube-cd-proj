181249cc3a037e20c99586e8511cc67fbe1338d4
Travis Holton <travis@ideegeo.com>
2015-11-02 08:49:39 +1300

    try and get the hostname for the local box

--- circle-test.sh
+++ circle-test.sh
@@ -3,7 +3,7 @@
 
 VERSION=$1
 
-
+POSTGRES_HOST=`echo /etc/hosts | cut -d " " -f 3`
 
 docker run \
     -h django-app \
@@ -12,7 +12,7 @@ docker run \
     -e DEPLOYMENT=development \
     -e DBNAME=circle_test \
     -e POSTGRES_USER=ubuntu \
-    -e POSTGRES_SERVICE_HOST=0.0.0.0 \
+    -e POSTGRES_SERVICE_HOST=$POSTGRES_HOST \
     -e POSTGRES_SERVICE_PORT=5432 \
     -e TEST=1 \
     $HOST/$IMAGE:$VERSION
