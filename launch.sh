84077b96df03de91f79a3280d6b48633e4d451e8
Travis Holton <travis@ideegeo.com>
2015-11-05 15:50:43 +1300

    hack startup to add to supervisor when running dev and staging

--- launch.sh
+++ launch.sh
@@ -4,22 +4,20 @@ sleep $START_DELAY
 
 # For docker-compose usage
 : ${DEPLOYMENT:=development}
-: ${VERBOSITY:=1}
 
 python3.5 manage.py migrate
-
-echo Ran migrations
-
 
 if [ "$TEST" = 1 ]; then
     echo Run tests
     ./run_tests.sh
 else
     if [ "$DEPLOYMENT" = "production" ]; then
-        echo Run production system
-        /usr/bin/supervisord --nodaemon -c /etc/supervisor/supervisord.conf
+        echo Run wsgi server
+        DJANGO_COMMAND="gunicorn kube_cd_project.wsgi"
     else
         echo Running python development server
-        python3.5 manage.py runserver -v $VERBOSITY 0.0.0.0:8000
+        DJANGO_COMMAND="python3.5 manage.py runserver 0.0.0.0:8000"
     fi
+    sed -i 's/DJANGO_COMMAND/'"$DJANGO_COMMAND"'/g' /etc/supervisor/conf.d/django.conf
+    /usr/bin/supervisord --nodaemon -c /etc/supervisor/supervisord.conf
 fi
