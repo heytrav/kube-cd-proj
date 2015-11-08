aa52c4f0c73d5e30e16ccfe76f03410e096289c6
Travis Holton <travis@ideegeo.com>
2015-11-09 07:40:52 +1300

    add template wrapper to import bootstrap stylesheet

--- kube_cd_project/settings.py
+++ kube_cd_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a$os(g8dn9tyv9u!$y^aaz^rl%qt54f7gz$jff-*(qq^pt9l%e'
+SECRET_KEY = os.environ.get('SECRET_KEY', None)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
