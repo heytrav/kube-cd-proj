803651c0ea57ce0f1dc524c00c7959b7023164a6
Travis Holton <travis@ideegeo.com>
2015-11-12 09:55:04 +1300

    adding cookbook setup

--- cookbook/views.py
+++ cookbook/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.views import generic
+from django.conf import settings
 
-# Create your views here.
+from .models import Meal
+from .forms import IngredientForm
+
+
+class IndexView(generic.ListView):
+
+    template_name = 'cookbook/index.html'
+    context_object_name = 'meal_list'
+
+    def get_context_data(self, **kwargs):
+        """Return the context data
+
+        :**kwargs: arguments
+        :returns: context object for view
+
+        """
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['version'] = settings.VERSION
+        return context
+
+    def get_queryset(self):
+        return Meal.objects.all()
+
+
+class IngredientsView(generic.ListView):
+    template_name = 'cookbook/ingredients.html'
+    context_object_name = 'ingredient_list'
+
+    def get_context_data(self, **kwargs):
+        """Return the context data
+
+        :**kwargs: arguments
+        :returns: context object for view
+
+        """
+        context = super(IngredientsView, self).get_context_data(**kwargs)
+        context['version'] = settings.VERSION
+        return context
+
+    def get_queryset(self):
+        return Ingredient.objects.all()
+
+
+
+
+class IngredientEditView(generic.edit.FormView):
+    template_name = 'cookbook/ingredients.html'
+    form_class = IngredientForm
+    success_url = '/success/'
