4c29f1004c9553c68e1f0ea6d0182cfe50621b22
Travis Holton <travis@ideegeo.com>
2015-11-11 15:53:10 +1300

    add models needed for cookbook and new migrations

--- cookbook/models.py
+++ cookbook/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class Meal(models.Model):
+
+    name = models.CharField(max_length=500)
+    stub = models.CharField(max_length=500)
+    instructions = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+
+    name = models.CharField(max_length=500)
+    stub = models.CharField(max_length=500)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Measurement(models.Model):
+    name = models.CharField(max_length=500)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    meal = models.ForeignKey(Meal)
+    ingredient = models.ForeignKey(Ingredient)
+    quantity = models.FloatField()
+    measurement = models.ForeignKey(Measurement)
