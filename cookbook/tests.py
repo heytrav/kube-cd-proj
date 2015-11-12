817d5b757fb7413be65ee5afb2c9440d7b9612b6
Travis Holton <travis@ideegeo.com>
2015-11-12 16:40:38 +1300

    test result of querying ingredients page

--- cookbook/tests.py
+++ cookbook/tests.py
@@ -1,6 +1,15 @@
 from django.test import TestCase
 from django.core.urlresolvers import reverse
 
+from .models import Ingredient
+
+def create_ingredient(name, stub, description):
+    """Create basic ingredient
+
+    """
+    return Ingredient.objects.create(name=name,
+                                     stub=stub,
+                                     description=description)
 
 class IngredientViewTests(TestCase):
 
@@ -9,3 +18,14 @@ class IngredientViewTests(TestCase):
         self.assertContains(response, 'No ingredients available.')
         self.assertEqual(response.status_code, 200)
         self.assertQuerysetEqual(response.context['ingredient_list'], [])
+
+    def test_ingredient_list_view_with_results(self):
+        create_ingredient('Onion', 'onion', 'Onions')
+        create_ingredient('Garlic', 'garlic', 'Goes with everything.')
+        response = self.client.get(reverse('cookbook:ingredients'))
+        print("Response: {!r}".format(response.context['ingredient_list']))
+        self.assertContains(response, 'Goes with everything.')
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(response.context['ingredient_list'].order_by('stub'),
+                                 ['<Ingredient: Garlic>',
+                                  '<Ingredient: Onion>'])
