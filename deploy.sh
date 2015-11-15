ab10bcea569354d552367fbd43517408924c82b9
Travis Holton <travis@ideegeo.com>
2015-11-16 10:04:23 +1300

    comment code in deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,15 @@ export VERSION=$1
 : ${CONTROLLER:=app}
 : ${CONTROLLER_FILE:=app-controller.yml}
 
-RELEASE=$( git tag | grep $VERSION )
+# Find out if we're on a pull request
 export BRANCH=$(git rev-parse --abbrev-ref HEAD | sed 's/\//-/g')
 PULL_REQUEST=$(echo ${BRANCH} | grep pull)
 
+
+# Find out if we're on an annotated tag
+RELEASE=$( git tag | grep $VERSION )
+
+# If on tag release to production
 if [  "$RELEASE" = "" ]; then
     KUBERNETES_CONTEXT=staging
 else
@@ -16,11 +21,18 @@ fi
 export DEPLOY_ENVIRONMENT=$KUBERNETES_CONTEXT
 KUBE_CMD=${KUBERNETES_ROOT:-~/kubernetes}/cluster/kubectl.sh
 $KUBE_CMD config use-context $KUBERNETES_CONTEXT
+
+
 CURRENT_CONTROLLER=$($KUBE_CMD get rc | grep ${CONTROLLER} | cut -f 1 -d " ")
 envsubst < kubernetes/$KUBERNETES_CONTEXT/${CONTROLLER_FILE}.template > $CONTROLLER_FILE
+
+# Save controller file for production releases to artifacts.
 if [ "$DEPLOY_ENVIRONMENT" = "production" ]; then
     cat $CONTROLLER_FILE > $CIRCLE_ARTIFACTS/${CONTROLLER}-${VERSION}.yml
 fi
+
+# Create new replication controller if app not running. Otherwise rolling
+# update to new version.
 if [ "$CURRENT_CONTROLLER" = "" ]; then
     $KUBE_CMD create -f ${CONTROLLER_FILE}
 else
