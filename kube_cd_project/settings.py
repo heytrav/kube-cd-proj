26d06b2782d05538bbdd0dc7db7a98885b2ac8f1
Travis Holton <travis@ideegeo.com>
2015-11-16 14:52:52 +1300

    don't need .org if in staging

--- kube_cd_project/settings.py
+++ kube_cd_project/settings.py
@@ -28,7 +28,7 @@ DEPLOY_ENVIRONMENT = os.environ.get('DEPLOYMENT', 'development')
 if DEPLOY_ENVIRONMENT != 'production':
     DEBUG = True
 
-ALLOWED_HOSTS = ['travissoto.com', 'travissoto.org']
+ALLOWED_HOSTS = ['travissoto.com']
 
 
 # Application definition
